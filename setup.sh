26404f95bf236216d9a9ca3231d2c829678e45b4
Asep Ready <asepdevops@gmail.com>
2025-05-20 14:30:13 +0700

    wazuh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,25 @@ echo 'server1_ip="10.0.0.20"' > /root/wazuh-gen-certs/server.lst
 echo 'dashboard_ip="10.0.0.30"' > /root/wazuh-gen-certs/dashboard.lst
 cd /root/wazuh-gen-certs; echo y | sh gen-certs.sh
 
+bastille cmd wazuh-indexer passwd -p 'sshroot'
 scp -r /root/wazuh-gen-certs/wazuh-certificates root@10.0.0.10:~
+bastille cmd wazuh-indexer mkdir /usr/local/etc/opensearch/certs
+bastille cmd wazuh-indexer cp /root/wazuh-certificates/admin.pem /usr/local/etc/opensearch/certs/
+bastille cmd wazuh-indexer cp /root/wazuh-certificates/admin-key.pem /usr/local/etc/opensearch/certs/
+bastille cmd wazuh-indexer cp /root/wazuh-certificates/indexer1.pem /usr/local/etc/opensearch/certs/
+bastille cmd wazuh-indexer cp /root/wazuh-certificates/indexer1-key.pem /usr/local/etc/opensearch/certs/
+bastille cmd wazuh-indexer cp /root/wazuh-certificates/root-ca.pem /usr/local/etc/opensearch/certs/
+bastille cmd wazuh-indexer chown -R opensearch:opensearch /usr/local/etc/opensearch/
+bastille cmd wazuh-indexer chmod -R 755 /usr/local/etc/opensearch/
+
 scp -r /root/wazuh-gen-certs/wazuh-certificates root@10.0.0.20:~
+bastille cmd wazuh-server passwd -p 'sshroot'
+
+bastille cmd wazuh-dashboard passwd -p 'sshroot'
 scp -r /root/wazuh-gen-certs/wazuh-certificates root@10.0.0.30:~
+bastille cmd wazuh-dashboard mkdir /usr/local/etc/opensearch-dashboards/certs
+bastille cmd wazuh-dashboard cp /root/wazuh-certificates/dashboard.pem /usr/local/etc/opensearch-dashboards/certs/
+bastille cmd wazuh-dashboard cp /root/wazuh-certificates/dashboard-key.pem /usr/local/etc/opensearch-dashboards/certs/
+bastille cmd wazuh-dashboard cp /root/wazuh-certificates/root-ca.pem /usr/local/etc/opensearch-dashboards/certs/
+bastille cmd wazuh-dashboard chown www:www /usr/local/etc/opensearch-dashboards/certs/root-ca.pem
+bastille cmd wazuh-dashboard chmod 755 /usr/local/etc/opensearch-dashboards/certs/root-ca.pem
